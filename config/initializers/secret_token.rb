97bb69d1095af864ef4f498ed92a5379fe6503c1
Pooja Gupta <pooja.gupta11@gmail.com>
2013-01-12 20:41:29 +0530

    Depot Start to Adding Page lLayout

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '62dc47258d20c61ab5506a81357786c66a03617c93cef0730ff4a67fa7ef639407f2563dbe669417e36f15770883f1c2d54ac1bbd81fcd32a12c60ba2fcac021'
